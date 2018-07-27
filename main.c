20ae72e81b6b7f583628c174d5992a5511b533ca
David Mendelovits <david.mendelovits@gmail.com>
2018-07-27 10:36:17 -0700

    push to mac

--- main.c
+++ main.c
@@ -9,13 +9,13 @@ int     main(void)
     unsigned int    u_int = 4294967295;
     char            *ptr = "Hello, dude";
 
-//    printf("\nprintf: char: %c", character);
+    printf("\nprintf: char: %c", character);
     ft_printf("\nft_printf: char:  %c", character);
-//    printf("\nprintf: string: %s", hello);
+    printf("\nprintf: string: %s", hello);
     ft_printf("\nft_printf: string: %s", hello);
-//    printf("\nprintf: int: %i", n);
+    printf("\nprintf: int: %i", n);
     ft_printf("\nft_printf: int: %i", n);
-//    printf("\nprintf: uint: %u", u_int);
+    printf("\nprintf: uint: %u", u_int);
     ft_printf("\nft_printf: uint: %u \n", u_int);
     ft_printf("\n %u - %s\n", u_int, hello);
     ft_printf("ptr: %p", ptr);
---
+++ src/print_binary.c
@@ -0,0 +1,6 @@
+#include "ft_printf.h"
+
+int     print_binary(va_list arg_list)
+{
+
+}
